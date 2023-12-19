6b87585c253fda17074d01d7d327bec87774bb01
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-19 15:58:33 -0800

    chore: cleanup debug

--- cmd/main.go
+++ cmd/main.go
@@ -12,9 +12,6 @@ import (
 func main() {
 	opts := optmod.ParseOptions()
 
-	slog.Info("hi")
-	slog.Error("example fail")
-
 	if opts.Verbose || opts.LogFormat != "" {
 		if opts.LogFormat == "json" {
 			goldbug.SetDefaultLoggerJson(slog.LevelDebug)
@@ -22,9 +19,6 @@ func main() {
 			goldbug.SetDefaultLoggerText(slog.LevelDebug)
 		}
 	}
-
-	slog.Info("hi")
-	slog.Error("example fail")
 
 	code := underbeing.Main(opts)
 	os.Exit(code)
