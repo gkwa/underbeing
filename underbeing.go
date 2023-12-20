a96e7d5a86d12634fdcec84ec1e16ba46a46ae40
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-20 09:17:51 -0800

    refactor: repoPath is more clear than currentDir

--- underbeing.go
+++ underbeing.go
@@ -25,18 +25,18 @@ func Main(opts *optmod.Options) int {
 }
 
 func run(opts *optmod.Options) error {
-	currentDir, err := os.Getwd()
+	repoPath, err := os.Getwd()
 	if err != nil {
 		return fmt.Errorf("failed to get current directory: %w", err)
 	}
 
-	isGitRepo, err := isGitRepository(currentDir)
+	isGitRepo, err := isGitRepository(repoPath)
 	if err != nil {
 		return fmt.Errorf("failed to check Git repository: %w", err)
 	}
 
 	if isGitRepo {
-		slog.Debug("the current directory is a Git-initialized directory", "path", currentDir)
+		slog.Debug("the current directory is a Git-initialized directory", "path", repoPath)
 	} else {
 		return fmt.Errorf("the current directory is not a Git-initialized directory")
 	}
@@ -44,7 +44,7 @@ func run(opts *optmod.Options) error {
 	repoName := opts.RepoName
 	if repoName == "" {
 		// Use the current directory name as the repository name
-		_, repoName = filepath.Split(currentDir)
+		_, repoName = filepath.Split(repoPath)
 	}
 
 	slog.Debug("debug reponame", "repo", repoName)
@@ -78,7 +78,7 @@ func run(opts *optmod.Options) error {
 
 	slog.Debug("check githubuser", "githubuser", username)
 
-	if err := setPushDefaultToCurrent(currentDir); err != nil {
+	if err := setPushDefaultToCurrent(repoPath); err != nil {
 		slog.Error("setPushDefaultToCurrent", "error", err)
 	}
 
