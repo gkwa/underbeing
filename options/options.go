5133301a773f3ebd14bf361115927c21734e2cf6
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-21 08:14:10 -0800

    style: remove noise

--- options/options.go
+++ options/options.go
@@ -9,13 +9,14 @@ type Options struct {
 	RepoName   string
 }
 
-// ParseOptions parses the command-line options and returns an Options struct
 func ParseOptions() *Options {
 	opts := &Options{}
 
 	flag.BoolVar(&opts.Verbose, "verbose", false, "Enable verbose output")
 	flag.BoolVar(&opts.Verbose, "v", false, "Enable verbose output (shorthand)")
+
 	flag.StringVar(&opts.LogFormat, "log-format", "", "Log format (text or json)")
+
 	flag.StringVar(&opts.GithubUser, "github-user", "", "GitHub username (overrides GITHUB_USER environment variable)")
 	flag.StringVar(&opts.RepoName, "repo-name", "", "GitHub repository name (overrides the current directory name)")
 
